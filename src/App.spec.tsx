7fbe4edb55e9a24bb939ee66ef4c373da9bfd3b6
johelder <johelder.arj@gmail.com>
2023-01-12 21:37:56 -0300

    style: prettier fix

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,10 +1,10 @@
-import {render} from "@testing-library/react";
-import { App } from "./App";
+import { render } from '@testing-library/react';
+import { App } from './App';
 
 describe('App Component', () => {
   it('should render the Hello Elofy title correctly', () => {
     const screen = render(<App />);
 
-    expect(screen.getByText('Hello Elofy')).toBeInTheDocument()
+    expect(screen.getByText('Hello Elofy')).toBeInTheDocument();
   });
-})
+});
