c9be93405a2f08397a316072a91203a7ccd607b8
johelder <johelder.araujo@pede.ai>
2023-01-13 16:23:05 -0300

    feat: redirect to details

--- src/components/NavBarItem/index.tsx
+++ src/components/NavBarItem/index.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import { INavBarItemProps } from './types';
 
+import './styles.css';
+
 export const NavBarItem = ({ detail }: INavBarItemProps) => {
   return (
     <div>
-      {/* <Link to={`/details/${detail}`}>{detail}</Link> */}
-      <a href="">teste</a>
+      <Link to={`/details/${detail}`} className="NavItem">
+        {detail}
+      </Link>
     </div>
   );
 };
